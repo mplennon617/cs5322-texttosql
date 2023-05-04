8c5524f8a6cae47679982e17c523cd9bc7d83e3e
Jurech <jamesurech@gmail.com>
2023-05-04 13:42:42 -0500

    Associating each DB id with the Schema objects

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import json
 
 class schema:
-    def __init__ (self):
+    def __init__ (self, id):
+        self.id = id
         self.tableNames = []
 
 
 with open('./spider-data/tables.json', 'r') as f:
-  data = json.load(f)
+  schemaDoc = json.load(f)
+
+with open('./spider-data/train_spider.json', 'r') as f:
+  queryDoc = json.load(f)
 
 schemas = []
 
-for idx, iSchema in enumerate(data):
-    test = schema()
+for idx, iSchema in enumerate(schemaDoc):
+    test = schema(iSchema['db_id'])
     for columnName in iSchema['table_names']:
         test.tableNames.append(columnName)
-    print("Schema", idx, ":", test.tableNames)
+    #print("Schema", idx, ":", test.id, ":", test.tableNames)
     schemas.append(test)
 
 print("Hello TextToSQL")

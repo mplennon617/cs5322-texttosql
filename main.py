ad1c90f4287dad1e4e553397d5f8d4c2d642ac47
Jurech <jamesurech@gmail.com>
2023-05-04 13:20:35 -0500

    Simple read to get all column names out of the SQL

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import json
+
+with open('./spider-data/tables.json', 'r') as f:
+  data = json.load(f)
+
+for columnName in data[0]['column_names']:
+  print(columnName[1])
+
 print("Hello TextToSQL")

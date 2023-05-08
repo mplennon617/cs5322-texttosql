4a4f1f11a41156773bfef332918b630f78a0d803
Jurech <jamesurech@gmail.com>
2023-05-08 17:15:30 -0500

    Read in level 2 information

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ class schema:
     def __init__ (self, id):
         self.id = id
         self.tableNames = []
+        self.columnNames = []
+        self.columnAtributes = []
 
 
 # Loading Spider Data
@@ -20,15 +22,22 @@ for idx, iSchema in enumerate(schemaDoc):
     dbid = iSchema['db_id']
     test = schema(dbid)
     for columnName in iSchema['table_names_original']:
-        test.tableNames.append(columnName)
+      test.tableNames.append(columnName)
+    for columnName in iSchema['column_names_original']:
+      test.columnNames.append(columnName[1])
+      test.columnAtributes.append(columnName[0])
     #print("Schema", idx, ":", test.id, ":", test.tableNames)
+    #print("Columns:", test.columnNames)
+    #print("Atributes:", test.columnAtributes)
+    #print()
     schemas[dbid] = test
 
 for idx, iquery in enumerate(queryDoc):
    # Initialize a blank array with the length of the number of tables
    tablesArray = [0] * len(schemas[iquery['db_id']].tableNames)
+   columnsArray = [0] * len(schemas[iquery['db_id']].columnNames)
    # Get all the tokens from teh particular query we are looking at
-   queryTokens = iquery['query_toks']
+   queryTokens = iquery['query_toks_no_value']
 
     # TODO: Use spacy to tag all table names using named entity recognition (NER).
 
@@ -41,7 +50,13 @@ for idx, iquery in enumerate(queryDoc):
            # If the table names match, indicate it in tablesArray
            if tableName.lower() == queryToken.lower():
               tablesArray[jdx] = 1
-   print(tablesArray)
+      else:
+         # Search through all of the column names in the database we are looking at
+         for jdx, tableName in enumerate(schemas[iquery['db_id']].columnNames):
+           # If the table names match, indicate it in tablesArray
+           if tableName.lower() == queryToken.lower():
+              columnsArray[jdx] = 1
+   print(tablesArray, columnsArray)
    #print(schemas[iquery['db_id']].id)
    #print(iquery['query_toks'])
 
